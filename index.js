959087444091cb000805afeabb906904093a59a0
ram kumar <72140644+ramprogrammer3@users.noreply.github.com>
2022-10-03 07:49:44 +0530

    part9

--- index.js
+++ index.js
@@ -1,108 +1,94 @@
 
-let userData = [
-    {
-        "id": 1,
-        "name": "Leanne Graham",
-        "username": "Bret",
-        "email": "Sincere@april.biz",
-        "address": {
-        "street": "Kulas Light",
-        "suite": "Apt. 556",
-        "city": "Gwenborough",
-        "zipcode": "92998-3874",
-        "geo": {
-        "lat": "-37.3159",
-        "lng": "81.1496"
-        }
-        },
-        "phone": "1-770-736-8031 x56442",
-        "website": "hildegard.org",
-        "company": {
-        "name": "Romaguera-Crona",
-        "catchPhrase": "Multi-layered client-server neural-net",
-        "bs": "harness real-time e-markets"
-        }
-        },
-        {
-        "id": 2,
-        "name": "Ervin Howell",
-        "username": "Antonette",
-        "email": "Shanna@melissa.tv",
-        "address": {
-        "street": "Victor Plains",
-        "suite": "Suite 879",
-        "city": "Wisokyburgh",
-        "zipcode": "90566-7771",
-        "geo": {
-        "lat": "-43.9509",
-        "lng": "-34.4618"
-        }
-        },
-        "phone": "010-692-6593 x09125",
-        "website": "anastasia.net",
-        "company": {
-        "name": "Deckow-Crist",
-        "catchPhrase": "Proactive didactic contingency",
-        "bs": "synergize scalable supply-chains"
-        }
-        }
-];
+// console.log("Node js tutorial")
 
-
-const express = require("express");
-const port = 8080;
+const express = require('express');
 const app = express();
-const http = require("http");
-console.log(http.STATUS_CODES)
+const port = 8080;
 
-app.use(express.json())
 app.get("/",(req,res)=>{
-    res.json("hello this is express js tutorial ")
-})
-
-app.get("/user",(req,res)=>{
-    res.status(302).json(userData);
-})
-
-app.post("/signup",(req,res)=>{
-    console.log(req.body);
-    res.status(202).json("Data found ");
+    res.sendFile(`${__dirname}/public/index.html`)
 })
 
 
-app.post("/signup",(req,res)=>{
-    let {name, email, password} = req.body;
-    console.log(name);
-    console.log(email);
-    console.log(password);
-    res.status(302).json("data found ");
+app.use(express.static(__dirname+"/public"))
+app.get("/",(req,res)=>{
+    res.sendFile(`${__dirname}/public/index.html`)
 })
 
-app.post("/signup",(req,res)=>{
-    let {name, email, password} = req.body;
-    console.log(req.body);
-    if(name){
-        return res.status(202).json("data found ");
+
+
+mySql connection 
+
+const mysql = require("mysql")
+const db = mysql.createConnection({
+    host : "localhost",
+    user : "root",
+    password : "123456A$"
+})
+db.connect((err)=>{
+    if(!err){
+        console.log("dB connection successful")
     }else{
-        res.status(204).json("data not found ");
+        console.log("DB connection failed ")
     }
 })
-app.use("*",(req,res)=>{
-    res.status(404).json({Error : "not found "});
-    
+app.get("/",(req,res)=>{
+    let sqlQuery = `SELECT * FROM world.city;`;
+    db.sqlQuery(sqlQuery,(err,resultData)=>{
+        if(!err){
+            res.json(resultData);
+        }
+    })
 })
 
 
-const schoolRoutes = require('./views/schoolRoutes');
-const ecommerceRoutes = require('./views/eCommerce');
-app.use(express.json());
-app.use(schoolRoutes);
-app.use(ecommerceRoutes)
 
-app.use("*",(req,res)=>{
-    res.status(404).json({Error : "not found "});
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://ramkumar:334300@cluster0.lerh1za.mongodb.net/?retryWrites=true&w=majority")
+.then(()=>{
+    console.log("DB connection successful");
+}).catch(()=>{
+    console.log("DB connection failed ");
 })
+
+
+
+
+app.set("view engine",'ejs');
+app.get("/",(req,res)=>{
+    res.render("index.ejs")
+})
+app.get("/",(req,res)=>{
+    res.render("index.ejs",{
+        title : "welcome to ejs",
+        name : ["ram","shyam","mohan"]
+    })
+})
+
+
+const mysql = require('mysql');
+const db = mysql.createConnection({
+    host : "localhost",
+    user : 'root',
+    password : "123456A$"
+})
+db.connect((err)=>{
+    if(err){
+        console.log("DB connection failed ");
+    }else{
+        console.log("DB connection  successful ");
+    }
+})
+app.get("/table",(req,res)=>{
+    let sqlQuery = `SELECT * FROM world.city;`;
+    db.query(sqlQuery,(err,result)=>{
+        if(!err){
+            res.send(result);
+        }
+    })
+})
+
 
 app.listen(port,()=>{
-    console.log(`server is runnin on port ${port}`);
+    console.log(`server is running on port ${port}`);
 })
