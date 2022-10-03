1f9f2e5118abe0a3504ea47f3a869c5ce756d083
ram kumar <72140644+ramprogrammer3@users.noreply.github.com>
2022-10-03 20:28:16 +0530

    part12

--- index.js
+++ index.js
@@ -3,11 +3,21 @@
 const express = require('express');
 const app = express();
 const port = 8080;
-const http = require("http");
-console.log(http.STATUS_CODES)
+const mongoose = require("mongoose");
 const userRoutes = require("./views/userRoutes");
+
+const uri = "mongodb+srv://ramkumar:334300@cluster0.lerh1za.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(uri,{useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=>{
+    console.log("DB connection successful");
+}).catch(()=>{
+    console.log("DB connection failed ");
+})
+
 app.use(express.json());
-app.use(userRoutes);
+app.use(userRoutes);   
+
+
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`);
 })
--- views/userRoutes.js
+++ views/userRoutes.js
@@ -1,81 +1,300 @@
 
-const express = require("express");
+
+const express = require("express")
 const router = express.Router();
-const {ObjectId} = require("mongodb");
-const {getCollection} = require("../dbConnection");
-router.get("/getUsers",async(req,res)=>{
-    let collection = await getCollection();
-    let userData = await collection.find().toArray();
-    res.json(userData);
-})
-router.get("/getUser/:id",async(req,res)=>{
-    const collection = await getCollection();
-    try {
-        const user = await collection.findOne({_id : ObjectId(req.params.id)})
-        res.status(302).json(user)
-    } catch (err) { 
-    }
-})
-router.post("/registerUser",async(req,res)=>{
-    let collection = await getCollection();
-    let result = await collection.insertOne(req.body)
-    res.status(201).json(result);
-})
-router.post("/registerUser",async(req,res)=>{
-    let {name, email, password, confirmPassword} = req.body;
-    if(!name || !email || !password || !confirmPassword){
-        return res.status(206).json({Error : "field missing "});
-    }else if(password !== confirmPassword){
-        return res.status(417).json({Error : "password and confirmPassword does not match"});
-    }else{
-        try{
-            let collection =  await getCollection();
-            const result = await collection.insertOne(req.body)
-            return res.status(201).json({Success : "user register successfully"})
-        }catch(err){
-            return res.status(500).json({Error : err})
+const userModel = require("../models/userSchema")
+
+router.post("/registerUser",(req,res)=>{
+    const user = new userModel(req.body)
+    user.save((err,savedUser)=>{
+        if(err){
+            res.json(err)
+        }else{
+            res.json(savedUser)
         }
-    }
+    })
 })
-router.post("/registerUser",async(req,res)=>{
-    let {name, email, password, confirmPassword} = req.body;
-    if(!name || !email || !password || !confirmPassword){
-        return res.status(206).json({Error : "field missing "});
-    }else if(password !== confirmPassword){
-        return res.status(417).json({Error : "password and confirmPassword does not match"});
-    }else{
-        try{
-            let collection =  await getCollection();
-            const result = await collection.insertOne({name,email,password})
-            return res.status(201).json({Success : "user register successfully"})
-        }catch(err){
-            return res.status(500).json({Error : err})
+
+
+router.get("/getAllUsers",(req,res)=>{
+    userModel.find((err,users)=>{
+        if(err){
+            res.json(err)
+        }else{
+            res.json(users)
         }
-    }
+    })
 })
-router.put("/updateUser/:id",async(req,res)=>{
-    let collection = await getCollection();
-    try {
-        let result = await collection.updateOne({_id : ObjectId(req.params.id)},{$set : req.body});
-        return res.json(result)
-    } catch (err) {
-        return res.status(404).json({Error : "user not found with this id "})
-    }
-})
-router.delete("/deleteUser/:id",async(req,res)=>{
-    let collection = await getCollection();
-    try {
-        let result = await collection.deleteOne({_id : ObjectId(req.params.id)})
-        if(result.deletedCount == 0){
-            return res.status(404).json({Error : "no user found with this id "});
+
+router.get("/getAllUser",(req,res)=>{
+    userModel.findOne({_id : "630334569962479efaec1c41"},(err,users)=>{
+        if(err){
+            res.json(err)
+        }else{
+            res.json(users)
         }
-        return res.json(result);
-    } catch (err) {
-        res.json(err);
-    }
+    })
 })
-router.get("*",(req,res)=>{
-    res.status(404).json({Error : "sorry this api is not created "});
+
+
+router.get("/getAllUser",(req,res)=>{
+    userModel.find({name : "mohit"},(err,users)=>{
+        if(err){
+            res.json(err)
+        }else{
+            res.json(users)
+        }
+    })
+})
+
+
+router.get("/getAllUser",(req,res)=>{
+    userModel.findOne({name : "mohit"},(err,users)=>{
+        if(err){
+            res.json(err)
+        }else{
+            res.json(users)
+        }
+    })
+})
+
+
+router.get("/getUser/:name",(req,res)=>{
+    userModel.findOne({name : req.params.name},(err,users)=>{
+        if(err){
+            res.json(err)
+        }else{
+            res.json(users)
+        }
+    })
+})
+
+
+router.get("/getUser/:id",(req,res)=>{
+    userModel.findOne({ _id : req.params.id},(err,users)=>{
+        if(err || !users){
+            if(!users){
+               return res.status(404).json({Error : "no user found with this id"})
+            }
+          return  res.json(err)
+        }else{
+          return  res.json(users)
+        }
+    })
+})
+
+
+
+router.put("/updateUser/:id",(req,res)=>{
+    userModel.updateOne({ _id : req.params.id},req.body,(err,result)=>{
+        if(err){
+            res.json(err)
+        }else{
+            res.json(result)
+        }
+    })
+})
+
+
+router.put("/updateUser/:name",(req,res)=>{
+    userModel.updateMany({ name : req.params.name},req.body,(err,result)=>{
+        if(err){
+            res.json(err)
+        }else{
+            res.json(result)
+        }
+    })
+})
+
+
+router.put("/updateUser/:name",(req,res)=>{
+    userModel.updateMany({ name : req.params.name},req.body,(err,result)=>{
+        if(err){
+            res.json(err)
+        }else{
+            res.json(result)
+        }
+    })
+})
+
+
+
+
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// restApi
+
+
+
+
+const express = require("express")
+const router = express.Router();
+const userModel = require("../models/userSchema")
+const { body, validationResult } = require('express-validator');
+
+router.post("/users",(req,res)=>{
+    const user = new userModel(req.body)
+    user.save((err,savedUser)=>{
+        if(err){
+            res.json(err)
+        }else{
+            res.json(savedUser)
+        }
+    })
+})
+
+router.get("/users",(req,res)=>{
+    userModel.find((err,users)=>{
+        if(err){
+            res.json(err)
+        }else{
+            res.json(users)
+        }
+    })
+})
+
+router.get("/users/:id",(req,res)=>{
+    userModel.findOne({ _id : req.params.id},(err,users)=>{
+        if(err || !users){
+            if(!users){
+               return res.status(404).json({Error : "no user found with this id"})
+            }
+          return  res.json(err)
+        }else{
+          return  res.json(users)
+        }
+    })
+})
+
+router.put("/users/:name",(req,res)=>{
+    userModel.updateMany({ name : req.params.name},req.body,(err,result)=>{
+        if(err){
+            res.json(err)
+        }else{
+            res.json(result)
+        }
+    })
+})
+
+module.exports = router;
+
+
+
+const express = require("express")
+const router = express.Router();
+const userModel = require("../models/userModel")
+const { body, validationResult } = require('express-validator');
+
+router.post("/users",
+body('email').isEmail(),    
+(req,res)=>{
+    const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+          }
+    const user = new userModel(req.body)      
+    user.save((err,savedUser)=>{
+        if(err){
+            res.json(err)
+        }else{
+            res.json(savedUser)
+        }
+    })
+})
+
+
+
+router.post("/users",
+body('email', "not in the email format").isEmail(), 
+body('phoneNo',"phoneNo must be 10 characters").isLength({ min: 10 }),
+(req,res)=>{
+    const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+          }
+    const user = new userModel(req.body)      
+    user.save((err,savedUser)=>{
+        if(err){
+            res.json(err)
+        }else{
+            res.json(savedUser)
+        }
+    })
+})
+
+
+router.post("/users",
+body('email', "not in the email format").isEmail(), 
+body('phoneNo',"phoneNo must be 10 characters").isLength({ min: 10 }),
+(req,res)=>{
+    const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+          }
+    const user = new userModel(req.body)      
+    user.save((err,savedUser)=>{
+        if(err){
+            res.json(err)
+        }else{
+            res.json(savedUser)
+        }
+    })
+})
+
+router.get("/users",(req,res)=>{
+    userModel.find((err,users)=>{
+        if(err){
+            res.json(err)
+        }else{
+            res.json(users)
+        }
+    })
+})
+
+router.get("/users/:id",(req,res)=>{
+    userModel.findOne({ _id : req.params.id},(err,users)=>{
+        if(err || !users){
+            if(!users){
+               return res.status(404).json({Error : "no user found with this id"})
+            }
+          return  res.json(err)
+        }else{
+          return  res.json(users)
+        }
+    })
+})
+
+router.put("/users/:name",(req,res)=>{
+    userModel.updateMany({ name : req.params.name},req.body,(err,result)=>{
+        if(err){
+            res.json(err)
+        }else{
+            res.json(result)
+        }
+    })
+})
+
+router.delete("/users/:id",(req,res)=>{
+    userModel.deleteOne({ _id : req.params.id},(err,result)=>{
+        if(err){
+            res.json(err)
+        }else{
+            res.json(result)
+        }
+    })
 })
 
 module.exports = router;
