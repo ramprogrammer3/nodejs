15562b49b5ae9cdea2bf7a191b991bce42a4f88e
ram kumar <72140644+ramprogrammer3@users.noreply.github.com>
2022-10-03 20:54:00 +0530

    part15

--- index.js
+++ index.js
@@ -1,95 +1,32 @@
-
-const express = require('express')
+const express = require("express")
+const app = express()
 const port = 8080;
-const app = express();
-const http = require('http').createServer(app);
-const io = require("socket.io")(http);
+const http = require("http").createServer(app)
+const io = require('socket.io')(http);
 
 app.get("/",(req,res)=>{
-    res.sendFile(__dirname + "/public/index.html")
+    res.sendFile(__dirname+"/public/index.html")
 })
 
-// io.on("connection",(socket)=>{
-//     console.log(`user connected`)
-// })
+let userCount = 0;
 
-// io.on("connection",(socket)=>{
-//     console.log(`user connected`)
-//     socket.on("message",(msg)=>{
-//         console.log(msg)
-//     })
-// })
-
-
-// io.on("connection",(socket)=>{
-//     console.log(`user connected`)
-//     socket.on("message",(msg)=>{
-//         io.emit("message",msg)
-//     })
-// })
-
-
-// io.on("connection",(socket)=>{
-//     console.log(`user connected`)
-//     socket.on("message",(name,message)=>{
-//         console.log(`${message} send by ${name}`)
-//     })
-// })
-
-
-// io.on("connection",(socket)=>{
-//     console.log(`user connected`)
-//     socket.on("message",(name,message)=>{
-//         console.log(`${message} send by ${name}`);
-//         io.emit("message",name,message)
-//     });
-//     socket.on("deleteMessage",(id)=>{
-//         io.emit("deleteMessage",id)
-//     })
-// })
-
-
-
-
-// io.on("connection",(socket)=>{
-//     io.emit("connection")
-//     console.log(`user connected`)
-//     socket.on("message",(name,message)=>{
-//         console.log(`${message} send by ${name}`);
-//         io.emit("message",name,message)
-//     });
-//     socket.on("deleteMessage",(id)=>{
-//         io.emit("deleteMessage",id)
-//     })
-// })
-
-
-
-
-let usersConnected = 0;
 io.on("connection",(socket)=>{
-    usersConnected++;
-    io.emit("connection",usersConnected)
-    console.log(`user connected`)
+    userCount++;
+    io.emit("connection",userCount)
+    console.log("user connected ")
     socket.on("message",(name,message)=>{
-        console.log(`${message} send by ${name}`);
         io.emit("message",name,message)
-    });
+    })
     socket.on("deleteMessage",(id)=>{
         io.emit("deleteMessage",id)
     })
 
     socket.on("disconnect",()=>{
-        console.log("user disconnected")
-        usersConnected--;
-        io.emit("connection",usersConnected)
+        userCount--;
+        io.emit("connection",userCount)
     })
-    
 })
 
-
-
-
 http.listen(port,()=>{
-    console.log(`server is running on port ${port}`)
+    console.log(`server is running on  port ${port}`)
 })
